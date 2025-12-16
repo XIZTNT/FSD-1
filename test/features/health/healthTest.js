a6b75474c6011ed9188c480deafea8bcbe993ad0
xiztnt <chadolosky@gmail.com>
2025-12-15 22:36:55 -0500

    hello, status, and error tests passing, just not able to run within vscode terminal, command has to be executed within local bash terminal and otherwise works and passes

--- test/features/health/healthTest.js
+++ test/features/health/healthTest.js
@@ -1,21 +1,48 @@
 const chai = require('chai');
 const sinon = require('sinon');
-const HealthController = require('../src/features/health/health.controller');
-const ResponseUtil = require('../src/shared/utils/response-util').ResponseUtil;
+//Changed file path to have extra "../" to match test folder structure
+const HealthController = require('../../../src/features/health/health.controller');
+const ResponseUtil = require('../../../src/shared/utils/response-util').ResponseUtil;
 
-describe('HealthController',()=>{
+describe('HealthController', () => {
   afterEach(() => {
     sinon.restore();
   });
 
-  describe('#helloWorld()',()=>{
-    it('respond with Hello World',(done)=>{
-      sinon.stub(ResponseUtil,'respondOk').callsFake((res,data,message)=>{
-        chai.assert.equal(message,'Hello World');
+  describe('#helloWorld()', () => {
+    it('respond with Hello World', (done) => {
+      sinon.stub(ResponseUtil, 'respondOk').callsFake((res, data, message) => {
+        chai.assert.equal(message, 'Hello World');
         done();
       });
-      
+
       void HealthController.helloWorld();
     });
   });
+
+  //Status Testing
+  describe('#status()', () => {
+    it('respond with status ok', (done) => {
+      sinon.stub(ResponseUtil, 'respondOk').callsFake((res, data, message) => {
+        chai.assert.equal(message, 'Status OK');
+        done();
+      });
+
+      void HealthController.status();
+    });
+  });
+
+  //Error Testing
+  describe('#error()', () => {
+    it('respond with error message', (done) => {
+      const errorMessage = 'Test Error Message';
+      sinon.stub(ResponseUtil, 'respondError').callsFake((res, error, message) => {
+        chai.assert.equal(message, errorMessage);
+        done();
+      });
+
+      void HealthController.error(new Error(errorMessage));
+    });
+  });
+
 });
